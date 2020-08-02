b11bc60c23cc9380b4807dec4f4af449f4e1bf3c
Cheryl Daniels <cheryld433@gmail.com>
2020-08-01 20:15:09 -0400

    Updates

---
+++ precache-manifest.99f0d06181257f092e18b165f94ecf25.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3477b5699261f6c937fb889ed3f83c39",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "ad785842eaafbb11d5e0",
+    "url": "/portfolio-react/static/css/2.11b894de.chunk.css"
+  },
+  {
+    "revision": "31ce41541f4cf38ac028",
+    "url": "/portfolio-react/static/css/main.238a3728.chunk.css"
+  },
+  {
+    "revision": "ad785842eaafbb11d5e0",
+    "url": "/portfolio-react/static/js/2.1cbc55bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-react/static/js/2.1cbc55bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ce41541f4cf38ac028",
+    "url": "/portfolio-react/static/js/main.dac1da30.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "1dc17bfcbee886a7995ca7e92c5ce64f",
+    "url": "/portfolio-react/static/media/Homemade.1dc17bfc.png"
+  },
+  {
+    "revision": "380ea3f972f582a665a221fc5ba7934d",
+    "url": "/portfolio-react/static/media/Me.380ea3f9.jpeg"
+  },
+  {
+    "revision": "7e772f4ed0853a2ceba3e990a32ac1e4",
+    "url": "/portfolio-react/static/media/budget-tracker.7e772f4e.png"
+  },
+  {
+    "revision": "612b1486578a39accb7bae43a640a663",
+    "url": "/portfolio-react/static/media/burger.612b1486.png"
+  },
+  {
+    "revision": "36dee2f0c565ea347140e21884366781",
+    "url": "/portfolio-react/static/media/cheryl_daniels.36dee2f0.pdf"
+  },
+  {
+    "revision": "6fad98cb3bf25450712baba4fd9607f6",
+    "url": "/portfolio-react/static/media/contact-background.6fad98cb.png"
+  },
+  {
+    "revision": "3ad2a364b3103d64542307172250a2d0",
+    "url": "/portfolio-react/static/media/homeBackground1.3ad2a364.png"
+  },
+  {
+    "revision": "477918f3557d7026a5065ae9dc32c659",
+    "url": "/portfolio-react/static/media/notetaker.477918f3.png"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/portfolio-react/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/portfolio-react/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/portfolio-react/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/portfolio-react/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "30ab80cb91fc3597580d38801a1fcf69",
+    "url": "/portfolio-react/static/media/react-directory.30ab80cb.png"
+  },
+  {
+    "revision": "bcbaa58d5e4bcaaa9ff993e02e9012b2",
+    "url": "/portfolio-react/static/media/red-planet.bcbaa58d.png"
+  }
+]);
